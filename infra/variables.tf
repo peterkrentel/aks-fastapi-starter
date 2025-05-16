7d11e384d927e324e33b0bf484f0b40e90ef129a
Peter Krentel <peterkrentel@gmail.com>
2025-05-16 09:35:01 -0500

    fuck

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,10 +1,20 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
+variable "client_id" {
+  description = "Azure Client ID (Service Principal)"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret (Service Principal)"
   type        = string
 }
 
 variable "tenant_id" {
   description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
   type        = string
 }
 
