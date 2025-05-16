9e683c15313ba5c936f88a7d873b6bedb2e05c97
Peter Krentel <peterkrentel@gmail.com>
2025-05-16 10:11:50 -0500

    annoying vars

--- infra/main.tf
+++ infra/main.tf
@@ -13,8 +13,8 @@ terraform {
 provider "azurerm" {
   features {}
     subscription_id = var.subscription_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
+    #client_id       = var.client_id
+    #client_secret   = var.client_secret
     tenant_id       = var.tenant_id
 
 }
